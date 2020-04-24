c63587adcb3978e12f253ad4d5603cdb3f0bf2dc
AndresHF <andresherrerof@gmail.com>
2020-04-24 18:30:30 +0200

    [Hotfix] Corrected prod image url

---
+++ out/_next/static/6VZIcHiClguNqoGjB9ZOp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b]}}("static\u002Fcss\u002Fstyles.c92e9d88.chunk.css","static\u002Fchunks\u002Fstyles.389155ca3a6e5015dc4b.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
