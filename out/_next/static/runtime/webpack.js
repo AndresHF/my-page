ab682490a985a44658236edf253dd06586de9c2e
AndresHF <andresherrerof@gmail.com>
2020-05-07 14:23:05 +0200

    [DEPLOY] Corrected flex position @ Menu

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f0155e3a89aaac357775";
+/******/ 	var hotCurrentHash = "1bb7cc427affcfe6100a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
