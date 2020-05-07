fc3addd4a03cf6e9282ede22cabfadb440f4a012
AndresHF <andresherrerof@gmail.com>
2020-05-07 12:38:48 +0200

    [DEPLOY] Better efficiency at Matrix

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e7e6c6201c2ba1189214";
+/******/ 	var hotCurrentHash = "4ed662eaf733dde0ed68";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
